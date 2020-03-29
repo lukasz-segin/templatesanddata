4f8e0e05bc2678a0e5ce1da0ba5ddbf2f20d8555
angrysegin <lukasz-segin@o2.pl>
2020-03-29 18:13:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid text-center">
+    <div class="bg-primary text-white m-2 p-3">
+      <h3>Produkt: {{ name }}</h3>
+    </div>
+    <button v-on:click="handleClick" class="btn btn-primary">
+      Wciśnij mnie
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyComponent',
+    data() {
+      return {
+        name: "Kamizelka ratunkowa"
+      }
+    },
+    methods: {
+      handleClick() {
+        // nie rób nic
+      }
+    }
+  }
+</script>
+<style>
+  h3:first-child {
+    background-color: aquamarine;
+    padding: 10px;
+    color: black;
+  }
+</style>
