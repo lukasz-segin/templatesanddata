e1fb5b56719cc54d838fe775df2ff34391893806
angrysegin <lukasz-segin@o2.pl>
2020-04-01 18:46:19 +0200

    Przeglądanie tablicy. Stosowanie aliasu. Okreslanie klucza.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="container-fluid text-center">
-    <div class="bg-primary text-white m-2 p-3">
-      <h3>Produkt: {{ name }}</h3>
+  <div class="container-fluid">
+    <h2 class="btn-primary text-white text-center p-3">
+      Produkty
+    </h2>
+    <table class="table table-sm table-bordered table-striped text-left">
+      <tr>
+        <th>Nazwa</th>
+        <th>Cena</th>
+      </tr>
+      <tbody>
+      <tr v-for="p in products" v-bind:key="p.name">
+        <td>{{ p.name }}</td>
+        <td>{{ p.price | currency }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <div class="text-center">
+      <button v-on:click="handleClick" class="btn btn-primary">
+        Wciśnij mnie
+      </button>
     </div>
-    <button v-on:click="handleClick" class="btn btn-primary">
-      Wciśnij mnie
-    </button>
   </div>
 </template>
 
@@ -14,20 +28,29 @@
     name: 'MyComponent',
     data() {
       return {
-        name: "Kamizelka ratunkowa"
+        products: [
+          {name: "Kajak", price: 275},
+          {name: "Kamizelka ratunkowa", price: 48.95},
+          {name: "Piłka nożna", price: 19.50}
+        ]
+      }
+    },
+    filters: {
+      currency(value) {
+        return new Intl.NumberFormat("pl-PL",
+            { style: "currency", currency: "PLN",}).format(value);
       }
     },
     methods: {
       handleClick() {
-        // nie rób nic
+        this.products.push(this.products.shift());
       }
     }
+    // document.querySelector("tbody > tr").id = "tagged"
   }
 </script>
 <style>
-  h3:first-child {
-    background-color: aquamarine;
-    padding: 10px;
-    color: black;
+  #tagged {
+    background-color: coral;
   }
 </style>
