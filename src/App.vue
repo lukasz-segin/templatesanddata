2328a3096cb13b095a57c01187c1a9b125fb86ca
angrysegin <lukasz-segin@o2.pl>
2020-04-02 20:03:04 +0200

    Stosowanie własciwosci obliczanych z dyrektywą v-for. Stronicowanie danych.

--- src/App.vue
+++ src/App.vue
@@ -5,52 +5,63 @@
     </h2>
     <table class="table table-sm table-bordered table-striped text-left">
       <tr>
-        <th>Indeks</th>
-        <th>Klucz</th>
         <th>Nazwa</th>
         <th>Cena</th>
       </tr>
       <tbody>
-      <tr v-for="(p, key, i) in products" v-bind:key="p.name">
-        <td>{{ i + 1 }}</td>
-        <td>{{ key }}</td>
+      <tr v-for="p in pageItems" v-bind:key="p.name">
         <td>{{ p.name }}</td>
         <td>{{ p.price | currency }}</td>
       </tr>
       </tbody>
     </table>
     <div class="text-center">
-      <button v-on:click="handleClick" class="btn btn-primary">
-        Wciśnij mnie
+      <!-- eslint-disable-next-line vue/require-v-for-key -->
+      <button v-for="i in pageCount" v-on:click="selectPage(i)" class="btn btn-secondary m-1"
+              v-bind:class="{'bg-primary' : currentPage == i}"> {{ i }}
       </button>
     </div>
   </div>
 </template>
 
 <script>
-  import Vue from "vue";
   export default {
     name: 'MyComponent',
     data() {
       return {
-        products: {
-          "kayak":{name: "Kajak", price: 275},
-          22:{name: "Kamizelka ratunkowa", price: 48.95},
-          3:{name: "Piłka nożna", price: 19.50},
-          "4":{name: "Chorągiewki narożne", price: 39.95},
-          // 5:{name: "Stadion", price: 79500}
-        }
+        pageSize: 3,
+        currentPage: 1,
+        products: [
+          {name: "Kajak", price: 275},
+          {name: "Kamizelka ratunkowa", price: 48.95},
+          {name: "Piłka nożna", price: 19.50},
+          {name: "Chorągiewki narożne", price: 39.95},
+          {name: "Stadion1", price: 179500},
+          {name: "Stadion2", price: 279500},
+          {name: "Stadion3", price: 379500},
+          {name: "Stadion4", price: 479500},
+          {name: "Stadion5", price: 579500},
+        ]
+      }
+    },
+    computed: {
+      pageCount() {
+        return Math.ceil(this.products.length / this.pageSize);
+      },
+      pageItems() {
+        let start = (this.currentPage - 1) * this.pageSize;
+        return this.products.slice(start, start + this.pageSize);
       }
     },
     filters: {
       currency(value) {
         return new Intl.NumberFormat("pl-PL",
-            { style: "currency", currency: "PLN",}).format(value);
+            {style: "currency", currency: "PLN",}).format(value);
       }
     },
     methods: {
-      handleClick() {
-        Vue.set(this.products, 5, { name: "Buty do biegania", price: 100 });
+      selectPage(page) {
+        this.currentPage = page;
       }
     }
     // document.querySelector("tbody > tr").id = "tagged"
@@ -60,9 +71,11 @@
   [odd] {
     background-color: lightblue !important;
   }
+
   tbody > tr:nth-child(even) {
     background-color: coral;
   }
+
   tbody > tr:nth-child(odd) {
     background-color: lightblue;
   }
