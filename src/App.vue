c0e6a5dfa9e222bedc804ae8a8ddf2b6064cddc0
angrysegin <lukasz-segin@o2.pl>
2020-04-04 19:21:58 +0200

    Połączenie zdarzeń, metod i elementów powtarzanych. Nasłuchiwanie wielu zdarzeń z tego samego elementu.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="container-fluid">
-    <div class="bg-primary text-white m-2 p-3 text-center">
-      <h3 v-on:click="handleEvent('Piłka nożna', $event)">{{ name }}</h3>
-    </div>
-
-    <div class="bg-primary text-white m-2 p-3 text-center">
-      <h3 v-on:click="handleEvent('Stadion', $event)">{{ name }}</h3>
-    </div>
+    <h3 class="bg-primary text-white text-center mt-2 p-2">{{ message }}</h3>
+    <table class="table table-sm table-striped table-bordered">
+      <tr>
+        <th>Indeks</th>
+        <th>Nazwa</th>
+        <th>Akcje</th>
+      </tr>
+      <tr v-for="(name, index) in names" v-bind:key="name">
+        <td>{{ index }}</td>
+        <td>{{ name }}</td>
+        <td>
+          <button class="btn btn-sm bg-primary text-white"
+                  v-on:click="handleClick(name)"
+                  v-on:mousemove="handleMouseEvent(name, $event)"
+                  v-on:mouseleave="handleMouseEvent(name, $event)"
+          >
+            Wybierz
+          </button>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -15,12 +29,22 @@
     name: 'MyComponent',
     data() {
       return {
-        name: "Kamizelka ratunkowa"
+        counter: 0,
+        message: "Gotowy",
+        names: ["Kajak", "Kamizelka ratunkowa", "Piłka nożna", "Stadion"]
       }
     },
     methods: {
-      handleEvent(name, $event) {
-        this.name = `${name} - ${$event.type}`;
+      handleClick(name) {
+        this.message = `Wybierz: ${name}`;
+      },
+      handleMouseEvent(name, $event) {
+        if ($event.type === "mousemove") {
+          this.message = `Ruch w ${name} ${this.counter++}`;
+        } else {
+          this.counter = 0;
+          this.message = "Gotowy";
+        }
       }
     }
   }
