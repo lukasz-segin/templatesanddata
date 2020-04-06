faa6b1c13846cfaf690a92b327bf971d15ef9c94
angrysegin <lukasz-segin@o2.pl>
2020-04-06 19:20:40 +0200

    Tworzenie dwukierunkowych wiązań modeli. Dodawanie wiązania dwukierunkowego.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="container-fluid">
-    <div class="bg-primary p-4 text-white h3">
-      {{ message }}
+    <div class="bg-info mt-2 p-2 text-white">
+      Wartość: {{ dataValue }}
     </div>
-    <input class="form-control bg-light" placeholder="Wpisz tekst..." v-on:keydown.ctrl="handleKey"/>
+    <div class="bg-primary m-2 p-2 text-white">
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" v-on:change="handleChange" v-bind:checked="dataValue" />
+          Wartość danych
+        </label>
+      </div>
+    </div>
+    <div class="text-center m-2">
+      <button class="btn btn-secondary" v-on:click="reset">
+        Reset
+      </button>
+    </div>
   </div>
 </template>
 
@@ -12,12 +24,15 @@
     name: 'MyComponent',
     data() {
       return {
-        message: "Gotowy"
+        dataValue: false
       }
     },
     methods: {
-      handleKey($event) {
-        this.message = $event.key;
+      reset() {
+        this.dataValue = false;
+      },
+      handleChange($event) {
+        this.dataValue = $event.target.checked;
       }
     }
   }
